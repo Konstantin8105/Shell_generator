c6098c837790adf4fdca50a7a06fb8e4ee058924
konstantin8105 <konstantin8105@gmail.com>
2017-05-09 21:52:04 +0300

    add example for wall generator

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "github.com/Konstantin8105/Shell_generator/cliShellGenerator"
+import (
+	"github.com/Konstantin8105/Shell_generator/wallGenerator"
+)
 
 func main() {
-	cliShellGenerator.Cli()
+	//cliShellGenerator.Cli()
+
+	h := []wallGenerator.ShapePosition{
+		wallGenerator.ShapePosition{wallGenerator.Shape(wallGenerator.NewStiffiner(0.100, 0.010)), 0.3},
+	}
+
+	v := []wallGenerator.ShapePosition{
+		wallGenerator.ShapePosition{wallGenerator.Shape(wallGenerator.NewAngle(0.120, 0.012, wallGenerator.Type4)), 1.5},
+	}
+
+	_, _ = wallGenerator.WallGenerator(wallGenerator.Wall{Lenght: 2.0, Height: 1.0, Thk: 0.005}, v, h)
 }
